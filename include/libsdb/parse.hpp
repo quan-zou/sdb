0d2369b3932f0ee34efe610c88a881665668a123
quan-zou <4420134@qq.com>
2025-06-29 21:58:49 +0800

    adding memory and disassembly

--- include/libsdb/parse.hpp
+++ include/libsdb/parse.hpp
@@ -56,6 +56,28 @@ namespace sdb {
         return bytes;
     }
 
+    inline auto parse_vector(
+        std::string_view text) {
+        auto invalid = [] { sdb::error::send("Invalid format"); };
+
+        std::vector<std::byte> bytes;
+        const char* c = text.data();
+
+        if (*c++ != '[') invalid();
+
+        while (*c != ']') {
+            auto byte = sdb::to_integral<std::byte>({ c, 4 }, 16);
+            bytes.push_back(byte.value());
+            c += 4;
+
+            if (*c == ',') ++c;
+            else if (*c != ']') invalid();
+        }
+
+        if (++c != text.end()) invalid();
+
+        return bytes;
+    }
 
     template <class F>
     std::optional<F> to_float(std::string_view sv) {
